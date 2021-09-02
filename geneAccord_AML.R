7f88c60691b9ee2a6bd3ccd09bbbd2830297ca11
Jack Kuipers <31819930+jackkuipers@users.noreply.github.com>
2021-09-02 10:38:50 +0200

    Update with additional tests

--- geneAccord_AML.R
+++ geneAccord_AML.R
@@ -1,16 +1,18 @@
+# load libraries
+library(dplyr)
+library(purrr)
+library(tidyr)
 
 # test if GeneAccord is installed
 GeneAccordFlag <- FALSE
-try(GeneAccordFlag <- packageVersion("GeneAccord") > 0, silent = TRUE)
+try(GeneAccordFlag <- packageVersion("GeneAccord") > '1.0.0', silent = TRUE)
 
 if(GeneAccordFlag) {
   library(GeneAccord)
 } else {
-  library(dplyr)
-  library(purrr)
-  library(tidyr)
   source("./R/geneAccord_fns.R")
 }
+
 
 # read in data
 clone_df <- read.table("./data/AMLsubclones_trees.txt", header = TRUE)
@@ -18,11 +20,16 @@ clone_df <- read.table("./data/AMLsubclones_trees.txt", header = TRUE)
 cx_df <- clone_df_to_cx_df(clone_df)
 # select only gene pairs in more than 4 patients
 cx_selected <- filter_cx_df(cx_df, 4)
-# run the geneAccord testing
-cx_results <- geneAccord(cx_selected)
-# compare to the chi-squared asymptotics
-#cx_results$p_chisq <- pchisq(cx_results$LR, df = 1, lower.tail = FALSE)
-#cx_results$q_chisq <- p.adjust(cx_results$p_chisq, method = "BH")
 
+# run the standard geneAccord testing
+cx_results <- geneAccord(cx_selected)
 cx_results
 
+# run testing of patient selection
+pat_results <- geneAccord(cx_selected, test_type = "patient")
+pat_results
+
+# run combined testing of patient selection and 
+# location of gene pairs in those patients
+comb_results <- geneAccord(cx_selected, test_type = "combined")
+comb_results
