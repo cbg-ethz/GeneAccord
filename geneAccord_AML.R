f6c1a207d4f16c9fff19b1a41211e74a8bf1e1ed
Jack Kuipers <31819930+jackkuipers@users.noreply.github.com>
2021-09-10 11:56:46 +0200

    update naming

--- geneAccord_AML.R
+++ geneAccord_AML.R
@@ -5,14 +5,13 @@ library(tidyr)
 
 # test if GeneAccord is installed
 GeneAccordFlag <- FALSE
-try(GeneAccordFlag <- packageVersion("GeneAccord") > '1.0.0', silent = TRUE)
+#try(GeneAccordFlag <- packageVersion("GeneAccord") > '1.0.0', silent = TRUE)
 
 if(GeneAccordFlag) {
   library(GeneAccord)
 } else {
   source("./R/geneAccord_fns.R")
 }
-
 
 # read in data
 clone_df <- read.table("./data/AMLsubclones_trees.txt", header = TRUE)
@@ -21,15 +20,12 @@ cx_df <- clone_df_to_cx_df(clone_df)
 # select only gene pairs in more than 4 patients
 cx_selected <- filter_cx_df(cx_df, 4)
 
-# run the standard geneAccord testing
-cx_results <- geneAccord(cx_selected)
-cx_results
+# run the placement geneAccord testing
+(place_results <- geneAccord(cx_selected))
 
-# run testing of patient selection
-pat_results <- geneAccord(cx_selected, test_type = "patient")
-pat_results
+# run occurrence geneAccord testing
+(occur_results <- geneAccord(cx_selected, test_type = "occurrence"))
 
-# run combined testing of patient selection and 
+# run combined testing of occurrence in patients and 
 # location of gene pairs in those patients
-comb_results <- geneAccord(cx_selected, test_type = "combined")
-comb_results
+(comb_results <- geneAccord(cx_selected, test_type = "combined"))
